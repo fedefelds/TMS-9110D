1ece0ecbe0cb347ba2cb2d14ec20137edaf36d08
fbosio <fedebosio93@gmail.com>
2021-01-24 16:41:22 -0300

    Passed autopep8

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import re
+
+
 def string_parse(Frequency, Amplitude, Route_Point):
     Frequency_Unit = 'Hertz'
-    Amplitude_Unit ='m/s2 RMS'
+    Amplitude_Unit = 'm/s2 RMS'
     Lower_Tolerance = 0
     Upper_Tolerance = 0
     Sensor_Type = 'Voltage'
-    parse =''
+    parse = ''
     parse += str(Route_Point)
     parse += ','
     parse += str(Frequency)
     parse += ','
     parse += str(Frequency_Unit)
     parse += ','
-    parse += str(int(Amplitude*100)) #<--- int(x * 100), parece usar punto fijo
+    # <--- int(x * 100), parece usar punto fijo
+    parse += str(int(Amplitude*100))
     parse += ','
     parse += str(Amplitude_Unit)
     parse += ','
@@ -21,10 +24,11 @@ def string_parse(Frequency, Amplitude, Route_Point):
     parse += str(Upper_Tolerance)
     parse += ','
     parse += str(Sensor_Type)
-    
+
     return parse
 
-def preamble_parse(freq_values,amp_values):
+
+def preamble_parse(freq_values, amp_values):
     preamble = """Route Points,Route Name
     xxx,yyy
     Test Sensor Model
@@ -36,18 +40,18 @@ def preamble_parse(freq_values,amp_values):
 
 
     Route Point,Frequency,Frequency Unit,Amplitude,Amplitude Unit,Lower Tolerance,Upper Tolerance,Sensor Type"""
-    #Defino cuantos Route Points hay a partir de freq_values
+    # Defino cuantos Route Points hay a partir de freq_values
     preamble = re.sub("xxx", str(len(freq_values)), preamble)
 
-    #Defino el nombre de la rutina a partir del valor rms elegido
-    preamble = re.sub("yyy", 'RMS_'+str(amp_values[0])+"m/s2",preamble)
-    
-    return preamble
-    
-    
+    # Defino el nombre de la rutina a partir del valor rms elegido
+    preamble = re.sub("yyy", 'RMS_'+str(amp_values[0])+"m/s2", preamble)
 
-#Estos son los valores (redondeados) de frecuencia y amplitud sacados de la ISO-8041 
-freq_values_iso = [1,1.259,1.995,3.981,7.943,15.85,31.62,63.10,79.43,125.9]
+    return preamble
+
+
+# Estos son los valores (redondeados) de frecuencia y amplitud sacados de la ISO-8041
+freq_values_iso = [1, 1.259, 1.995, 3.981,
+                   7.943, 15.85, 31.62, 63.10, 79.43, 125.9]
 amp_values_iso = [0.1, 0.3, 0.5, 0.8, 1, 2.5, 5, 7.5, 10, 25, 50]
 
 for amplitud in amp_values_iso:
@@ -57,16 +61,12 @@ for amplitud in amp_values_iso:
     freq_values = freq_values_iso
     amp_values[0] = amplitud
 
-    
-
-
-
     # retocar aca
-    string = preamble_parse(freq_values_iso,amp_values)
+    string = preamble_parse(freq_values_iso, amp_values)
     for amp in amp_values:
         for freq in freq_values:
             string += '\n'
-            string += string_parse(freq,amp,Route_Point)
+            string += string_parse(freq, amp, Route_Point)
             Route_Point += 1
     # agrego \n
     string += """  
