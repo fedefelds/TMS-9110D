fe673361ffafa1bad4967767076b070a1dd27448
fedefelds <6933064+fedefelds@users.noreply.github.com>
2021-01-24 20:45:01 -0300

    defino pvc parse

--- main.py
+++ main.py
@@ -29,30 +29,31 @@ Route Point,Frequency,Frequency Unit,Amplitude,Amplitude Unit,Lower Tolerance,Up
     
     return preamble
     
-    
+def pvc_parse(amp_values_iso,freq_values_iso):
+    for amplitud in amp_values_iso:
+        Route_Point = 0
+        amp_values = [0]
+        # Estos son los valores de frecuencia y amplitud que uso en la rutina
+        freq_values = freq_values_iso
+        amp_values[0] = amplitud
+
+        #Para el valor de amplitud elegido, parseo el string nuevo
+        string = preamble_parse(freq_values_iso,amp_values)
+        for amp in amp_values:
+            for freq in freq_values:
+                string += '\n'
+                string += string_parse(freq,amp,Route_Point)
+                Route_Point += 1
+        # agrego \n final
+        string += '  \n'
+
+        # Escribo el archivo .pvc
+        filename = 'RMS_'+str(amp_values[0])+'ms2.pvc'
+        with open(filename, "w", newline='\r\n') as text_file:
+            text_file.write(string)    
 
 #Estos son los valores (redondeados) de frecuencia y amplitud sacados de la ISO-8041 
 freq_values_iso = [1,1.259,1.995,3.981,7.943,15.85,31.62,63.10,79.43,125.9]
 amp_values_iso = [0.1, 0.3, 0.5, 0.8, 1, 2.5, 5, 7.5, 10, 25, 50]
 
-for amplitud in amp_values_iso:
-    Route_Point = 0
-    amp_values = [0]
-    # Estos son los valores de frecuencia y amplitud que uso en la rutina
-    freq_values = freq_values_iso
-    amp_values[0] = amplitud
-
-    #Para el valor de amplitud elegido, parseo el string nuevo
-    string = preamble_parse(freq_values_iso,amp_values)
-    for amp in amp_values:
-        for freq in freq_values:
-            string += '\n'
-            string += string_parse(freq,amp,Route_Point)
-            Route_Point += 1
-    # agrego \n final
-    string += '  \n'
-
-    # Escribo el archivo .pvc
-    filename = 'RMS_'+str(amp_values[0])+'ms2.pvc'
-    with open(filename, "w", newline='\r\n') as text_file:
-        text_file.write(string)
+pvc_parse(amp_values_iso,freq_values_iso)
